10b1fc79afde62dfbe211240a6af4eeb3c4f27ee
flyinggor <flyinggor@qq.com>
2016-08-30 15:00:33 +0800

    更新文件

--- app/src/main/java/com/zwhkj/todaynews/todaynews/fragment/NewsFragment.java
+++ app/src/main/java/com/zwhkj/todaynews/todaynews/fragment/NewsFragment.java
@@ -1,18 +1,34 @@
 package com.zwhkj.todaynews.todaynews.fragment;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zwhkj.todaynews.todaynews.R;
 
 /**
  * 首页
  */
 public class NewsFragment extends BaseFragment{
     private Boolean isRead = true;
+    private View view;
     /**
      * 识别首页是否是当前显示的界面
      *
      * @param isFore
      */
     public void isForeground(boolean isFore) {
+
         this.isRead = isFore;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_news,container,false);
+        return view;
     }
 }
--- app/src/main/java/com/zwhkj/todaynews/todaynews/fragment/VideoFragment.java
+++ app/src/main/java/com/zwhkj/todaynews/todaynews/fragment/VideoFragment.java
@@ -1,10 +1,23 @@
 package com.zwhkj.todaynews.todaynews.fragment;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zwhkj.todaynews.todaynews.R;
 
 /**
  * 视频
  */
 public class VideoFragment extends BaseFragment{
-
+    private View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_video,container,false);
+        return view;
+    }
 }
